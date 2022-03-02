540f7c47b7f6f95922308fcad463d8116f0e8fdb
Peyton <54728307+19peytonsmith@users.noreply.github.com>
2022-03-02 17:25:52 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Words7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Words7.dir/main.cpp.obj"
+  "Words7.exe"
+  "Words7.exe.manifest"
+  "Words7.pdb"
+  "libWords7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Words7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
